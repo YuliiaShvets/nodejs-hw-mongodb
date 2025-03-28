0f9be68cf19159ec13f93d469c0be73d7f69baa7
YuliiaShvets <y.shvets85@gmail.com>
2025-03-28 18:50:34 +0100

    Initial commit

---
+++ src/scripts/addOneContact.js
@@ -0,0 +1,12 @@
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+
+export const addOneContact = async () => {
+    const contactList = await readContacts();
+    const newContacts = createFakeContact();
+    writeContacts([...contactList, newContacts]);
+};
+
+addOneContact();
