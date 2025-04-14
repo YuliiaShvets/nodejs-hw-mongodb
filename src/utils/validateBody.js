9d867a03ac0d7aa6842cb0144e75a56a25231f26
Yuliia Shvets <y.shvets85@gmail.com>
2025-04-14 21:50:06 +0200

    add .

--- src/utils/validateBody.js
+++ src/utils/validateBody.js
@@ -1,13 +1,15 @@
 import createHttpError from "http-errors";
 
-export const validateBody = (schema)=> async (req, res, next) => {
-    try{
-    await schema.validateAsync(req.body, {abortEarly: false,});
+export const validateBody = schema => {
+  const func = async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body, { abortEarly: false });
     next();
-} catch (err) {
-    const error = createHttpError(400, "Bad Request", {
-        errors: err.details,
-    });
-    next(error);
-}
+  } catch (error) {
+    next(createHttpError(400, error.message));
+  }
+};
+
+return func;
+
 };
